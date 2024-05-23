fa8b047076e2f23a4685bce4608a00e72d82aad5
Thuận Phát <phat06112003@gmail.com>
2024-05-23 21:24:30 +0700

    complete searchbar

--- Frontend/src/components/Header/Header.js
+++ Frontend/src/components/Header/Header.js
@@ -1,18 +1,46 @@
 import { useState, useEffect, useRef } from 'react';
+import { Link } from 'react-router-dom';
 import classNames from 'classnames/bind';
 import styles from './Header.module.scss';
 import { FaMagnifyingGlass } from "react-icons/fa6";
 import { IoClose } from "react-icons/io5";
 
 import logo from '../../assests/images/logo.png'
+import { tmdbClient } from '../../axios/AxiosClients';
 
 const cx = classNames.bind(styles);
 
 function Header({ refList }) {
 
+  const tempData = [
+    {
+      posterURL: 'https://image.tmdb.org/t/p/w92/z1p34vh7dEOnLDmyCrlUVLuoDzd.jpg',
+      name: 'Movie\'s name',
+      type: 'Movie'
+    }
+  ]
+
   const [searchText, setSearchText] = useState('');
   const [searchOpen, setSearchOpen] = useState(false);
+  const [searchResult, setSearchResult] = useState([]);
   const headerRef = useRef(null);
+
+  const getListFromURL = async (url, type) => {
+    const response = await tmdbClient.get(url);
+    let results = response?.data?.results.map((item) => {
+      return {
+        id: item.id,
+        type: type === 'tv' ? 'series' : 'movie',
+        name: item.title ? item.title : item.name,
+        originalLanguage: item.original_language,
+        imdb: null,
+        imgURL: 'https://image.tmdb.org/t/p/original' + item.backdrop_path,
+        poster: 'https://image.tmdb.org/t/p/w92' + item.poster_path
+      }
+    })
+
+    return results;
+  }
 
   useEffect(() => {
     const handleScroll = () => {
@@ -29,6 +57,26 @@ function Header({ refList }) {
       window.removeEventListener('scroll', handleScroll);
     };   
   },[]);
+
+  //Get result for searchbar
+  useEffect(() => {
+    const getResult = async () => {
+      let requestURL = 'https://api.themoviedb.org/3/search/movie?query=' + searchText + '&language=en';
+      const movieList = await getListFromURL(requestURL, 'movie');
+
+      requestURL = 'https://api.themoviedb.org/3/search/tv?query=' + searchText + '&language=en'
+      const seriesList = await getListFromURL(requestURL, 'tv');
+
+      const result = [...movieList, ...seriesList];
+      setSearchResult(result);
+    }
+
+    if (searchText.length === 0) {
+      setSearchResult([]);
+      return;
+    }
+    getResult();
+  }, [searchText])
 
   const scrollToTop = () => {
     window.scrollTo({
@@ -129,40 +177,25 @@ function Header({ refList }) {
             </div>
           </div>
 
-          <div className={cx('search-result-wrapper')}>
+          <div className={cx('search-result-wrapper', {'no-display': searchText.length === 0})}>
             <div className={cx('search-results')}>
-              <div className={cx('result-item')}>
-                <img className={cx('result-img')} src="https://image.tmdb.org/t/p/w92/z1p34vh7dEOnLDmyCrlUVLuoDzd.jpg" alt="" />
-                <div className={cx('result-info')}>
-                  <h3 className={cx('result-name')}>Movie's name</h3>
-                  <p className={cx('result-type')}>Movie</p>
-                </div>
-              </div>
-              <div className={cx('result-item')}>
-                <img className={cx('result-img')} src="https://image.tmdb.org/t/p/w92/z1p34vh7dEOnLDmyCrlUVLuoDzd.jpg" alt="" />
-                <div className={cx('result-info')}>
-                  <h3 className={cx('result-name')}>Movie's name</h3>
-                  <p className={cx('result-type')}>Movie</p>
-                </div>
-              </div>
-              <div className={cx('result-item')}>
-                <img className={cx('result-img')} src="https://image.tmdb.org/t/p/w92/z1p34vh7dEOnLDmyCrlUVLuoDzd.jpg" alt="" />
-                <div className={cx('result-info')}>
-                  <h3 className={cx('result-name')}>Movie's name</h3>
-                  <p className={cx('result-type')}>Movie</p>
-                </div>
-              </div>
-              <div className={cx('result-item')}>
-                <img className={cx('result-img')} src="https://image.tmdb.org/t/p/w92/z1p34vh7dEOnLDmyCrlUVLuoDzd.jpg" alt="" />
-                <div className={cx('result-info')}>
-                  <h3 className={cx('result-name')}>Movie's name</h3>
-                  <p className={cx('result-type')}>Movie</p>
-                </div>
-              </div>
+              {
+                searchResult.map((item, index) => {
+                  return (
+                    <Link key={index} className={cx('result-item')} to={'/' + item.type + '?id=' + item.id}>
+                      <img className={cx('result-img')} src={item.poster} alt={item.name} />
+                      <div className={cx('result-info')}>
+                        <h3 className={cx('result-name')}>{item.name}</h3>
+                        <p className={cx('result-type')}>{item.type}</p>
+                      </div>
+                    </Link>
+                  )
+                })
+              }
               
-              {/* <div className={cx('no-result')}>
+              <div className={cx('no-result', { 'no-display': searchResult.length > 0})}>
                 <p className={cx('no-result-text')}>No results</p>
-              </div> */}
+              </div>
             </div>
           </div>
           
