0f2c2935845704f962906d11998b5db858406bdb
Thuận Phát <phat06112003@gmail.com>
2024-07-31 08:51:05 +0700

    using react-toastify to display toast message

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,10 +1,25 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import AuthProvider from './hooks/authProvider';
 import { publicRoutes } from './routes';
 
 function App() {
   return (
     <div className="App">
+      <ToastContainer
+        position="top-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="dark"
+      />
+
       <AuthProvider>
         <Router>
             <Routes>
