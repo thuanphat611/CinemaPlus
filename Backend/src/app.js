a50e8e63ed4a8703f6110e43cd3e3fc1f198d22b
thuanphat611 <phat06112003@gmail.com>
2024-12-25 15:21:18 +0700

    Feat: Add global error handling middleware

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -23,9 +23,20 @@ app.use("/assets", express.static(path.join(__dirname, "../public")));
 
 app.use("/api/v1", routes);
 
+//Global error handling middleware
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal Server Error";
+
+  res.status(statusCode).json({
+    success: false,
+    message,
+  });
+});
+
 // send back a 404 error for any unknown api request
 app.use((req, res) => {
-  res.status(404).json({ message: "Not found" });
+  res.status(404).json({ success: false, message: "Endpoint not found" });
 });
 
 module.exports = app;
